71828e25e78c604ed2b975881416262de53ecb3f
Derek Allred <derekmallred@gmail.com>
2016-10-24 21:42:03 -0600

    DOM manipulation practice with Javascript

---
+++ Javascript-dom-manipulation/javascript.js
@@ -0,0 +1,26 @@
+document.getElementById("redCircle").onclick = function () {
+  document.getElementById("redCircle").style.display = "none";
+};
+document.getElementById("blueCircle").onclick = function () {
+  document.getElementById("blueCircle").style.display = "none";
+};
+document.getElementById("yellowCircle").onclick = function () {
+  document.getElementById("yellowCircle").style.display = "none";
+};
+
+document.getElementById("test-button").onclick = function () {
+  var inputText = "";
+  inputText = document.getElementById("text-input").value;
+  document.getElementById("test-text").innerHTML = inputText;
+};
+
+
+var items = ["Paint Ball Gun", "Shoes", "Laptop", "other"];
+items.push("Fire");
+items.splice(3,1);
+items.splice(items.length,0,"Test");
+items.splice(0,1,"First");
+
+for (var i = 0; i < items.length; i++) {
+  console.log(items[i]);
+}
