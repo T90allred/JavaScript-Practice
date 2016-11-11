e03a9738f1867a0c95f782e18aaa1c0d13e09162
Derek Allred <derekmallred@gmail.com>
2016-11-11 11:02:26 -0700

    Functions practice with fizzbuzz

---
+++ random.js
@@ -0,0 +1,17 @@
+function FizzBuzz(input, FizzBuzz, Fizz, Buzz){
+  if( input % 3 === 0 && input % 5 === 0 ){
+    console.log(FizzBuzz);
+  }
+  else if ( input % 3 === 0 ){
+    console.log(Fizz);
+  }
+  else if ( input % 5 === 0 ){
+    console.log(Buzz);
+  }
+  else{
+    console.log(input);
+  }
+}
+
+
+FizzBuzz(15, "Banana", "Orange", "Apple");
